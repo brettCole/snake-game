22a754e9e9ac442ed6e2b258c9224abe9c856dfd
brettCole <brett.cole@quadratec.com>
2022-02-14 06:39:30 -0500

    added canvasRef and useEffect updating based context

--- src/components/CanvasBoard.tsx
+++ src/components/CanvasBoard.tsx
@@ -29,6 +29,14 @@ export interface ICanvasBoard {
 }
 
 const CanvasBoard = ({ height, width }: ICanvasBoard) => {
+    const canvasRef = (useRef <HTMLCanvasElement) | (null > null);
+    const [ context, setContext ] = (useState < CanvasRenderingContext2D) | ( null > null);
+
+    useEffect(() => {
+        // Draw on canvas each time
+        setContext(canvasRef.current && canvasRef.current.getContext('2d')); //store in state variable
+    }, [context]);
+
     return (
         <canvas
             style = {{ border: '3px solid black' }}
